76f657f84f7e61ac566a113a8cff8001c1f064d2
anfri <f.cariaggi4@gmail.com>
2017-07-16 17:49:10 +0200

    Update testAnd.ml

--- Test_sets/testAnd.ml
+++ Test_sets/testAnd.ml
@@ -1,15 +1,11 @@
 
-(* esempio corretto *)
-(* Ipotetica sintassi concreta
-	true and true;
-*)
+(* Correct example *)
+(* Corresponding concrete syntax: true and true *)
 run (And(EBool(true), EBool(true))) ;;
 
 flush stdout ;;
 
-(* esempio sbagliato *)
-(* Ipotetica sintassi concreta
-	true and 32;
-*)
+(* Wrong example *)
+(* Corresponding concrete syntax: true and 32 *)
 run (And(EBool(true), EInt(32))) ;;
 
