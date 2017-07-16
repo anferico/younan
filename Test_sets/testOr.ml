0ca9198958918c13d3788f02c85ef633f4796703
anfri <f.cariaggi4@gmail.com>
2017-07-16 18:07:00 +0200

    Update testOr.ml

--- Test_sets/testOr.ml
+++ Test_sets/testOr.ml
@@ -1,14 +1,14 @@
 
-(* esempio corretto *)
-(* Ipotetica sintassi concreta
+(* Correct example *)
+(* Corresponding concrete syntax:
 	false or false;
 *)
 run (Or(EBool(false), EBool(false))) ;;
 
 flush stdout ;;
 
-(* esempio sbagliato *)
-(* Ipotetica sintassi concreta
+(* Wrong example *)
+(* Corresponding concrete syntax:
 	true or 25;
 *)
 run (Or(EBool(true), EInt(25))) ;;
