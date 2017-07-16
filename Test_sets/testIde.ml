ce93c886b00311c6826c9c04377a39ef015341a2
anfri <f.cariaggi4@gmail.com>
2017-07-16 17:58:06 +0200

    Update testIde.ml

--- Test_sets/testIde.ml
+++ Test_sets/testIde.ml
@@ -1,14 +1,14 @@
 
-(* esempio corretto *)
-(* Ipotetica sintassi concreta
+(* Correct example *)
+(* Corresponding concrete syntax:
 	let y = false in y;
 *)
 run (Let("y", EBool(false), Ide("y"))) ;; 
 
 flush stdout ;;
 
-(* esempio sbagliato *)
-(* Ipotetica sintassi concreta
+(* Wrong example *)
+(* Corresponding concrete syntax:
 	let y = false in z;
 *)
 run (Let("y", EBool(false), Ide("z"))) ;; 
